34640ebf4296777c777241e65518461c3cac7729
Nadezhda S <nad4454@yandex.ru>
2022-10-23 00:16:30 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "alfavit3_projekt"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+
+    val list = mutableListOf<Char>()
+    for (i in 'a'..'z'){
+        list.add(i)
+    }
+    print(list)
+}
